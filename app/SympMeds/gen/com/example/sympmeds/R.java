c51aae0d9562bde1fff8af764afec19fd3da47cf
Randall Rosing <rosingrr@email.uc.edu>
2013-07-16 15:19:08 -0400

    new activity

--- app/SympMeds/gen/com/example/sympmeds/R.java
+++ app/SympMeds/gen/com/example/sympmeds/R.java
@@ -65,30 +65,35 @@ public final class R {
         public static final int activity_drug_info=0x7f030000;
         public static final int activity_drug_list=0x7f030001;
         public static final int activity_main=0x7f030002;
+        public static final int activity_symptom_list=0x7f030003;
     }
     public static final class menu {
         public static final int drug_info=0x7f090000;
         public static final int drug_list=0x7f090001;
         public static final int main=0x7f090002;
+        public static final int symptom_list=0x7f090003;
     }
     public static final class string {
         public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060001;
         public static final int description=0x7f060003;
-        public static final int drug_name=0x7f06000c;
-        public static final int drug_selector=0x7f06000a;
+        public static final int drug_name=0x7f06000d;
+        public static final int drug_selector=0x7f06000b;
         public static final int empty=0x7f060000;
-        public static final int get_info=0x7f060009;
-        public static final int how_to=0x7f06000f;
-        public static final int instructions=0x7f060008;
+        public static final int get_info=0x7f06000a;
+        public static final int hello_world=0x7f060013;
+        public static final int how_to=0x7f060010;
+        public static final int instructions=0x7f060009;
         public static final int med_name=0x7f060004;
-        public static final int problems=0x7f06000e;
+        public static final int problems=0x7f06000f;
         public static final int search=0x7f060005;
+        public static final int start=0x7f060007;
         public static final int symptom_selector=0x7f060006;
-        public static final int title_activity_drug_info=0x7f06000b;
-        public static final int title_activity_drug_list=0x7f060007;
-        public static final int treats=0x7f06000d;
-        public static final int unknown=0x7f060010;
+        public static final int title_activity_drug_info=0x7f06000c;
+        public static final int title_activity_drug_list=0x7f060008;
+        public static final int title_activity_symptom_list=0x7f060012;
+        public static final int treats=0x7f06000e;
+        public static final int unknown=0x7f060011;
     }
     public static final class style {
         /** 
--- app/SympMeds/src/com/example/sympmeds/MainActivity.java
+++ app/SympMeds/src/com/example/sympmeds/MainActivity.java
@@ -5,11 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 public class MainActivity extends Activity {
-	public final static String DRUG_LIST = "com.example.sympmeds.LIST";
+	public final static String SYMPTOM_LIST = "com.example.sympmeds.LIST";
 	
 	/**onCreate() is called when the MainActivity activity is started,
 	 * which is at the start of the application.
@@ -19,12 +17,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-        		this, R.array.symptom_list, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+
     }
 
     /**
@@ -41,12 +34,12 @@ public class MainActivity extends Activity {
      * It calls up the DrugList activity with the list that is respect to
      * what is selected from the spinner in this activity.
      */
-    public void openList (View view) {
+    public void start(View view) {
     	// Do something in response to button}
-		Intent intent = new Intent(this, DrugList.class);
-		Spinner spinner = (Spinner) findViewById(R.id.spinner1);
-		String text = spinner.getSelectedItem().toString();
-		intent.putExtra(DRUG_LIST, text);
+		Intent intent = new Intent(this, SymptomList.class);
+		String blank = getResources().getString(R.string.empty);
+		intent.putExtra(SYMPTOM_LIST, blank);
+		
 		startActivity(intent);
     }
     
